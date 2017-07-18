2df99f2283d87253a16c5f159524f4f8029f499e
Miquel Llabres <miquelllabres@gmail.com>
2017-07-18 14:31:10 -0400

    initial commit

---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,9 @@
+export default function(){
+	return [
+	{title: 'Javascript:The good parts', pages:101},
+	{title: 'The Last Samurai', pages: 209},
+	{title: 'Harry Potter', pages: 400},
+	{title: 'Monsters', pages: 500},
+	{title: 'Html and Css Tutorial', pages:201}
+	]
+}
